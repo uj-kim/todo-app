8f3973216f6277c199d0261c4dfdb40bc262ab54
uj-kim <ninakyj@gmail.com>
2022-12-12 12:25:08 +0900

    feat: Todo 수정 및 삭제 기능 구현

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -30,4 +30,67 @@ router.post("/todo", async (req, res) => {
   }
 });
 
+// PATCH localhost: PORT/todo/:todoId = edit a specific todo (UPDATE)
+// 수정 성공시; true => res.send(true)
+// 수정 실패시; false => res.send(false)
+router.patch("/todo/:todoId", async (req, res) => {
+  try {
+    let [editTodo] = await Todo.update(
+      {
+        title: req.body.title,
+        done: req.body.done,
+      },
+      {
+        where: {
+          id: req.params.todoId,
+        },
+      }
+    );
+    //console.log(editTodo);
+    if (!editTodo) {
+      return res.send(false);
+    }
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+/* Todo.update({
+  title:req.body.title,
+  done: req.body.done,
+},
+{ where: {
+  id: req.params.todoId,
+},
+}).then((result) => {
+  console.log(result); //수정 성공시[1], 수정 실패시 [0] 출력
+
+  if(result[0]){
+  res.send(true);
+  } else{
+    res.send(false);
+  }
+});
+
+// 차이는 없으나, try-catch가 depth가 덜 깊기 때문에 선호된다.
+*/
+
+// DELETE localhost:PORT/todo/todoId
+router.delete("/todo/:todoId", async (req, res) => {
+  try {
+    let delTodo = await Todo.destroy({
+      where: {
+        id: req.params.todoId,
+      },
+    });
+    if (!delTodo) {
+      return res.send(false); 
+    }
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 module.exports = router;
