b1304a4bb933d0aaff429e6b274dd6ff33f90410
uj-kim <ninakyj@gmail.com>
2022-12-13 10:21:51 +0900

    feat: Todo 추가 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Todo from "./components/Todo";
+import AddTodo from "./components/AddTodo";
 
 function App() {
   const [todoItems, setTodoItems] = useState([
@@ -20,8 +21,24 @@ function App() {
     },
   ]);
 
+  const todoId = useRef(4);
+  // AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems(state)에 접근 불가능
+  // 상위 컴포넌트(App)은 AddTodo  컴포넌트에 접근 가능(부모 -> 자식)
+  // => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props 로 넘겨야함
+  const addItem = (newItem) => {
+    //newItem - {id: ?, title; ?, done : false}
+    //setTodoItems()
+
+    newItem.id = todoId.current++; //key를 위한 id설정
+    newItem.done = false; // done 초기화
+    // 기존 todoItems를 유지하고, 새로운 newItem을 추가
+    setTodoItems([...todoItems, newItem]);
+    //setTodoItems(todoItems.concat(newItem))과 같다
+  };
+
   return (
     <div className="App">
+      <AddTodo addItem={addItem} />
       {todoItems.map((item) => {
         return <Todo key={item.id} item={item} />; // map함수는 return을 한다,,,
       })}
---
+++ client/src/components/AddTodo.js
@@ -0,0 +1,39 @@
+// 1. 함수형 컴포넌트
+
+import { useState } from "react";
+
+// 2. input과 button을 가짐
+const AddTodo = ({ addItem }) => {
+  //사용자 입력을 저장할 객체
+  ///(id, title, done에 대한 정보를 저장해야해서 객체)oItem] = (useState({
+  const [todoItem, setTodoItem] = useState({
+    title: "",
+  });
+
+  const onButtonClick = () => {
+    //props로 받아온 addItem 함수 실행
+    addItem(todoItem); //{title: 'input 입력값'}
+    setTodoItem({ title: "" }); //input 초기화
+  };
+
+  const onEnterKey = (e) => {
+    if (e.key == "Enter") {
+      onButtonClick();
+    }
+  };
+
+  return (
+    <div className="AddTodo">
+      <input
+        type="text"
+        placeholder="Add your new Todo"
+        value={todoItem.title}
+        onChange={(e) => setTodoItem({ title: e.target.value })}
+        onKeyPress={onEnterKey}
+      />
+      <button onClick={onButtonClick}>ADD</button>
+    </div>
+  );
+};
+
+export default AddTodo;
